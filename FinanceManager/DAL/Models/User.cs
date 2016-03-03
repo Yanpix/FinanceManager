3f5a1c0b81da8fc562d43b59f49eb9e8e2725c4c
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2016-03-03 17:19:45 +0200

    Added Transaction and User models

---
+++ FinanceManager/DAL/Models/User.cs
@@ -0,0 +1,27 @@
+﻿using SQLite.Net.Attributes;
+using SQLiteNetExtensions.Attributes;
+using System.Collections.Generic;
+
+namespace FinanceManager.DAL.Models
+{
+    public class User
+    {
+        [PrimaryKey, AutoIncrement]
+        public int Id { get; set; }
+
+        public string Title { get; set; }
+
+        public string FirstName { get; set; }
+
+        public string LastName { get; set; }
+
+        [ForeignKey(typeof(MoneyBox))]
+        public int MoneyBoxId { get; set; }
+
+        [OneToMany]
+        public List<Transaction> Transactions { get; set; }
+
+        [ManyToOne]
+        public MoneyBox MoneyBox { get; set; }
+    }
+}
