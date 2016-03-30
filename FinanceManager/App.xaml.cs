5944eb080bf45d9a1c8036be7527bc3d8df94119
Pavlo Grushetsky <pavlo.grushetsky@gmail.com>
2016-03-30 18:38:09 +0300

    Created temporary pages to try on design layouts

--- FinanceManager/App.xaml.cs
+++ FinanceManager/App.xaml.cs
@@ -113,7 +113,8 @@ namespace FinanceManager
                 // When the navigation stack isn't restored navigate to the first page,
                 // configuring the new page by passing required information as a navigation
                 // parameter
-                if (!rootFrame.Navigate(typeof(LoginPage), e.Arguments))
+                //if (!rootFrame.Navigate(typeof(LoginPage), e.Arguments))
+                if (!rootFrame.Navigate(typeof(FinanceManager.View.Temporary.MainPage), e.Arguments))
                 {
                     throw new Exception("Failed to create initial page");
                 }
